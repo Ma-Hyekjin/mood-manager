8c5e8081b5b3d31b65681511960ba54762941a56
Ma-Hyekjin <a01080077504@gmail.com>
2025-11-21 14:51:27 +0900

    feat: LaTex update, .latex, .pdf

--- src/components/navigation/TopNav.tsx
+++ src/components/navigation/TopNav.tsx
@@ -7,15 +7,23 @@
 
   - app/layout.tsx 의 375px 중앙정렬 프레임 안의 최상단 네비게이션 바
   - Mood Manager 로고(svg)를 중앙 배치
-  - 좌우 버튼은 현재 없음 (향후 설정/알림 버튼 등 확장 가능)
 */
 
 "use client";
 
+import Image from "next/image";
+
 export default function TopNav() {
   return (
     <div className="w-full h-14 flex items-center justify-center border-b bg-white">
-      <img src="/mood-manager-logo.svg" className="h-7" alt="Mood Manager" />
+      <Image
+        src="/logos/mood-manager-logo.svg"
+        alt="Mood Manager"
+        width={150}
+        height={28}
+        className="h-7 w-auto"
+        priority
+      />
     </div>
   );
 }

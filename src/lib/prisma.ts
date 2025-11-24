d548305f1a4c54f10128f382cb228ac853b4d0d9
Ma-Hyekjin <a01080077504@gmail.com>
2025-11-24 19:56:58 +0900

    feat: update for deployment - AWS

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,1 +1,11 @@
 //Prisma 클라이언트
+
+// TODO: 백엔드 API 연동 시 Prisma 클라이언트 설정 필요
+// import { PrismaClient } from "@prisma/client";
+//
+// const prisma = new PrismaClient();
+//
+// export default prisma;
+
+// 빈 export로 모듈로 인식되도록 함
+export {};
